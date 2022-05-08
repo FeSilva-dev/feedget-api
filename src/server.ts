2a6e060f763828ef256d617d53ef78a8229a1a6d
FeSilva-dev <feliper.silva011@gmail.com>
2022-05-08 19:01:42 -0300

    add environment variable port

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,6 @@ app.use(express.json({limit: '50mb'}));
 app.use(routes);
 app.use(express.urlencoded({limit: '50mb'}));
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('Server started on port 3333');
 })
